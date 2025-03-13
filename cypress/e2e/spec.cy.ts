ec6f3804fafbc9a6afa61a7fc349992b312cee38
EitherSanchez <67474367+EitherSanchez@users.noreply.github.com>
2025-03-13 16:32:25 -0600

    Se agregaron selectores

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('/')
-    cy.get('h1').should('contain','Lista de Actividades');
+    //cy.wait(10_000);
+    //A wait to found the element cy.get('h1',{timeout: 1000})
+    cy.get('h1').should('contain','XLista de Actividades');
+    cy.contains('Agregar').click();
   })
 })
