6d070107f71c6dc3c7e9171be8e5ecd8d48b5b1e
EitherSanchez <ecabrales@arkusnexus.com>
2025-03-20 04:10:57 -0600

    Adding Edit Test

--- cypress/e2e/EditarActividad.cy.ts
+++ cypress/e2e/EditarActividad.cy.ts
@@ -8,10 +8,16 @@ describe('Editar una Actvidad', () => {
     //A wait to found the element cy.get('h1',{timeout: 1000})
     cy.validacionInicial();
 
-//Agregar Actividad
+//Editar Actividad
     cy.get(e2e.CAMPO_AGREGAR_ACTIVIDAD).type('Actividad de prueba');
     cy.get(e2e.BOTON_AGREGAR).click();
+    cy.get(e2e.BOTON_EDITAR).click();
+    cy.get(e2e.CAMPO_EDITAR_ACTIVIDAD).clear();
+    cy.wait(2_000);
+    cy.get(e2e.CAMPO_EDITAR_ACTIVIDAD).type('This is an edited text')
+    cy.wait(2_000);
+    cy.get(e2e.BOTON_GUARDAR).click();
 
-    cy.get('span').should('have.text','Actividad de prueba');
+    cy.get('span').should('have.text','This is an edited text');
   })
 })
--- cypress/support/constantes.ts
+++ cypress/support/constantes.ts
@@ -2,5 +2,8 @@ export const e2e = {
     TITULO_PRINCIPAL: '[data-cy="u3dd98"]',
     SUBTITULO: '[data-cy="u3dd91"]',
     CAMPO_AGREGAR_ACTIVIDAD: '[data-cy="u3dd92"]',
-    BOTON_AGREGAR: '[data-cy="u3dd93"]'
+    BOTON_AGREGAR: '[data-cy="u3dd93"]',
+    BOTON_EDITAR: '[data-cy="u3dd96"]',
+    CAMPO_EDITAR_ACTIVIDAD: '[data-cy="u3dd88"]',
+    BOTON_GUARDAR: '[data-cy="u3dd89"]'
 }
